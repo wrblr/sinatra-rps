ce65b54e9edd626aebe7ebf21444c7c0906418e8
wrblr <wrbell@uchicago.edu>
2024-10-26 04:31:07 +0000

    final version

--- app.rb
+++ app.rb
@@ -6,4 +6,57 @@ get("/") do
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
   "
+erb(:elephant)
+end
+
+get("/rock") do
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "tied"
+  elsif @comp_move == "paper"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+  
+  erb(:rock)
+end
+
+
+
+get("/paper") do
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "won"
+  elsif @comp_move == "paper"
+    @outcome = "tied"
+  else
+    @outcome = "lost"
+  end
+  
+  erb(:paper)
+end
+
+
+
+get("/scissors") do
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "lost"
+  elsif @comp_move == "paper"
+    @outcome = "won"
+  else
+    @outcome = "tied"
+  end
+  
+  erb(:scissors)
 end
